2540eb5e698640a18a24702bb4a876f6e4159132
wmarini <4212421+wmarini@users.noreply.github.com>
2024-06-30 23:03:28 -0700

    Adding support to OpenCV

--- mosaic/mosaic.cu
+++ mosaic/mosaic.cu
@@ -1,7 +1,7 @@
 #include "mosaic.h"
 #include <npp.h>
 #include <cuda_runtime.h>
-#include <opencv2/opencv.hpp>
+// #include <opencv2/opencv.hpp>
 #include <filesystem>
 #include <iostream>
 
@@ -32,6 +32,27 @@ void ProcessMosaic(const MosaicConfig&& config)
     //Create an 8 bit single channel image
     Mat img = imread("data/lenna.png", IMREAD_GRAYSCALE );
     std::cout << img.size() << std::endl;
+    const std::size_t img_size = img.total() * img.elemSize();
+
+    Npp8u* dSrc;
+    cudaMalloc<Npp8u>(&dSrc, img_size);
+
+    Npp8u* dDst;
+    cudaMalloc<Npp8u>(&dDst, img_size);
+
+    cudaMemcpy(dSrc, img.data, img_size, cudaMemcpyHostToDevice);
+
+    NppiSize size = {img.cols, img.rows};
+    const Npp8u value = 150;
+
+    nppiAddC_8u_C1IRSfs(value, dSrc, img.step, size, 1);
+
+    cudaMemcpy(img.data, dDst, img_size, cudaMemcpyDeviceToHost);
+
+    imwrite("data/lenna_out.png", img);
+
+    cudaFree(dSrc);
+    cudaFree(dDst);
 }
 
 }
