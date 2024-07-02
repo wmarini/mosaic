e2d5e98f9e6a84d5a3cb3ee8bcd9f713818b8417
wmarini <4212421+wmarini@users.noreply.github.com>
2024-07-01 21:18:17 -0700

    Building AppImage

--- mosaic/mosaic.cu
+++ mosaic/mosaic.cu
@@ -1,7 +1,7 @@
 #include "mosaic.h"
 #include <npp.h>
 #include <cuda_runtime.h>
-#include <opencv2/opencv.hpp>
+// #include <opencv2/opencv.hpp>
 #include <filesystem>
 #include <iostream>
 
@@ -28,31 +28,31 @@ void ProcessMosaic(const MosaicConfig&& config)
     std::cout << "# Images to be processed: " 
         << image_list.size() << std::endl;
 
-    using namespace cv;
-    //Create an 8 bit single channel image
-    Mat img = imread("data/lenna.png", IMREAD_GRAYSCALE );
-    std::cout << img.size() << std::endl;
-    const std::size_t img_size = img.total() * img.elemSize();
+    // using namespace cv;
+    // //Create an 8 bit single channel image
+    // Mat img = imread("data/lenna.png", IMREAD_GRAYSCALE );
+    // std::cout << img.size() << std::endl;
+    // const std::size_t img_size = img.total() * img.elemSize();
 
-    Npp8u* dSrc;
-    cudaMalloc<Npp8u>(&dSrc, img_size);
+    // Npp8u* dSrc;
+    // cudaMalloc<Npp8u>(&dSrc, img_size);
 
-    Npp8u* dDst;
-    cudaMalloc<Npp8u>(&dDst, img_size);
+    // Npp8u* dDst;
+    // cudaMalloc<Npp8u>(&dDst, img_size);
 
-    cudaMemcpy(dSrc, img.data, img_size, cudaMemcpyHostToDevice);
+    // cudaMemcpy(dSrc, img.data, img_size, cudaMemcpyHostToDevice);
 
-    NppiSize size = {img.cols, img.rows};
-    const Npp8u value = 150;
+    // NppiSize size = {img.cols, img.rows};
+    // const Npp8u value = 150;
 
-    nppiAddC_8u_C1IRSfs(value, dSrc, img.step, size, 1);
+    // nppiAddC_8u_C1IRSfs(value, dSrc, img.step, size, 1);
 
-    cudaMemcpy(img.data, dDst, img_size, cudaMemcpyDeviceToHost);
+    // cudaMemcpy(img.data, dDst, img_size, cudaMemcpyDeviceToHost);
 
-    imwrite("data/lenna_out.png", img);
+    // imwrite("data/lenna_out.png", img);
 
-    cudaFree(dSrc);
-    cudaFree(dDst);
+    // cudaFree(dSrc);
+    // cudaFree(dDst);
 }
 
 }
